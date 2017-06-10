455430c58fde00d0e7a0add5067b772472f79182
ndaniela996 <ndaniela123@gmail.com>
2017-06-10 15:01:14 +0200

    Main page Login Logout Registration

--- register.php
+++ register.php
@@ -13,20 +13,30 @@
     if($user!='' && $email!='' && filter_var($email,FILTER_VALIDATE_EMAIL) &&
         $pass!='' && $f_name!='' && $l_name!='')
     {
-        $password=$salt1.$pass.$salt2;
-        if($address!='')
+        $sql="SELECT email FROM users WHERE email='$email'";
+        $result=mysqli_query($connection,$sql);
+
+        if(mysqli_num_rows($result)>0)
         {
-            $sql="INSERT INTO users(username, password, email, first_name, last_name, address) VALUES('$user',md5('$password'),'$email','$f_name','$l_name','$address')";
+            echo "That email is already registered.";
         }
         else
         {
-            $sql="INSERT INTO users(username, password, email, first_name, last_name) VALUES('$user',md5('$password'),'$email','$f_name','$l_name')";
-        }
-        $result=mysqli_query($connection,$sql);
+            $password=$salt1.$pass.$salt2;
+            if($address!='')
+            {
+                $sql="INSERT INTO users(username, password, email, first_name, last_name, address) VALUES('$user',md5('$password'),'$email','$f_name','$l_name','$address')";
+            }
+            else
+            {
+                $sql="INSERT INTO users(username, password, email, first_name, last_name) VALUES('$user',md5('$password'),'$email','$f_name','$l_name')";
+            }
+            $result=mysqli_query($connection,$sql);
 
-        if(mysqli_affected_rows($connection)==0)
-        {
-            echo "An error occurred. Please try again.";
+            if(mysqli_affected_rows($connection)==0)
+            {
+                echo "An error occurred. Please try again.";
+            }
         }
     }
     else
